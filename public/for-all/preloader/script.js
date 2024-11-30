02b8c627c7ca11fa2121913bc419fa246a5459e2
Your Name <you@example.com>
2024-11-30 21:47:13 +0200

    preloader added

---
+++ public/for-all/preloader/script.js
@@ -0,0 +1,8 @@
+let loader = document.querySelector('.loader-conteiner');
+
+window.addEventListener('load', () => {
+    loader.classList.add('active');
+    setTimeout(() => {
+        loader.remove
+    }, 300);
+});
